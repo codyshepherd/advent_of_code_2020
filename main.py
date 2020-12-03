744f1b5e6b6bf90604d4d6b6c2b92a3ac553c732
Cody Shepherd <cody.shepherd@gmail.com>
2020-12-03 08:50:40 -0800

    Solutions to day 3

--- main.py
+++ main.py
@@ -19,6 +19,40 @@ class Expense(object):
                 return reduce(lambda a,b: a*b, summands, 1)
 
 
+class SledMap(object):
+
+    def __init__(self, filename: str):
+        with open(filename, 'r') as fh:
+            self.input = fh.readlines()
+
+    def prepare_map(self, right:int, down:int):
+        total_required_length = (right*down*len(self.input))+1
+        newmap = []
+        for line in self.input:
+            newline = ''
+            while len(newline) < total_required_length:
+                newline += line.replace('\n', '')
+            newmap.append(newline)
+        return newmap
+
+    def count_trees_on_slope(self, right: int, down: int)->int:
+        slope_map = self.prepare_map(right, down)
+        count = 0
+
+        for h, line in enumerate(slope_map):
+            index = (right*down*h) if right > down else (right*(h//down))
+            newstring = line[:index] + '[' + line[index] + ']' + line[index+1:]
+            if h != 0 and h%down == 0 and line[index] == '#':
+                count += 1
+        return count
+
+    def count_several_slopes(self, slope_list: (int, int))-> list:
+        trees = []
+        for (right, down) in slope_list:
+            trees.append(self.count_trees_on_slope(right, down))
+        return trees
+
+
 class Passwords(object):
 
     def __init__(self, filename: str):
@@ -81,6 +115,16 @@ def day_2(fname: str):
     print('Part 2: {}'.format(pw.count_valid_pwds_new()))
 
 
+def day_3(fname: str):
+    print('Day 3\n==========')
+    m = SledMap(fname)
+    print('Part 1: {}'.format(m.count_trees_on_slope(3, 1)))
+    trees = m.count_several_slopes([(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
+    print(trees)
+    prod = reduce(lambda a,b: a*b, trees, 1)
+    print('Part 2: {}'.format(prod))
+
+
 @click.command()
 @click.argument('day')
 @click.argument('fname', type=click.Path())
@@ -89,6 +133,7 @@ def main(day, fname):
     commands = {
         '1': lambda x: day_1(fname),
         '2': lambda x: day_2(fname),
+        '3': lambda x: day_3(fname),
     }
 
     commands.get(day, lambda x: print(f'No day {x}'))(fname)
